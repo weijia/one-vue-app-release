7f4d902be64d2804265355ae17a0d6504b96d166
weijia <richardwangwang@gmail.com>
2021-12-26 22:28:52 +0800

    Fix hostname issue

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3b308314779a0123d01b7891f708954b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.7245bb44de8dbd56a98241f4abedcbb0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // Files to cache
 const cacheName = 'js13kPWA-v1';
